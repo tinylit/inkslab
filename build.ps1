07b5a9af4bdac012249df4a13970cf7184bc103e
影子和树 <m.hyl@foxmail.com>
2022-12-03 17:35:58 +0800

    添加CI/CD。

---
+++ build.ps1
@@ -0,0 +1,31 @@
+[CmdletBinding(PositionalBinding = $false)]
+param(
+    [bool] $CreatePackages = $true
+)
+
+Write-Host "Run Parameters:" -ForegroundColor Cyan
+Write-Host "  CreatePackages: $CreatePackages"
+Write-Host "  dotnet --version:" (dotnet --version)
+
+$packageOutputFolder = "$PSScriptRoot\.nupkgs"
+$projectsToBuild =
+    'Inkslab',
+    'Inkslab.Config',
+    'Inkslab.Json',
+    'Inkslab.Map',
+    'Inkslab.DI'
+
+mkdir -Force $packageOutputFolder | Out-Null
+Write-Host "Clearing existing $packageOutputFolder..." -NoNewline
+Get-ChildItem $packageOutputFolder | Remove-Item
+Write-Host "done." -ForegroundColor "Green"
+
+Write-Host "Building all packages" -ForegroundColor "Green"
+if ($CreatePackages) {
+	foreach ($project in $projectsToBuild) {
+		Write-Host "Packing $project (dotnet pack)..." -ForegroundColor "Magenta"
+		dotnet pack ".\src\$project\$project.csproj" -c Release --no-build -o $packageOutputFolder /p:NoPackageAnalysis=true  /p:CI=true
+		Write-Host "Packaged $project (dotnet pack)..." -ForegroundColor "Green"
+	}
+}
+Write-Host "Build Complete." -ForegroundColor "Green"
