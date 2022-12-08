df5e12a5ee0bccc86795277df2dfbdd346979058
影子和树 <m.hyl@foxmail.com>
2022-12-08 17:45:15 +0800

    添加 MD5 和常用的对称加密算法。

---
+++ src/Inkslab/Extentions/CryptoExtentions.cs
@@ -0,0 +1,319 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace System
+{
+    /// <summary>
+    /// 加密模式。
+    /// </summary>
+    public enum CryptoKind
+    {
+        /// <summary>
+        /// 有效的 KEY 与 IV 长度，以英文字符为单位： KEY（Min:8 Max:8 Skip:0），IV（8）。
+        /// </summary>
+        DES,
+        /// <summary>
+        /// 有效的 KEY 与 IV 长度，以英文字符为单位： KEY（Min:16 Max:24 Skip:8），IV（8）。
+        /// </summary>
+        TripleDES,
+        /// <summary>
+        /// 有效的 KEY 与 IV 长度，以英文字符为单位： KEY（Min:5 Max:16 Skip:1），IV（8）。
+        /// </summary>
+        RC2,
+        /// <summary>
+        /// 有效的 KEY 与 IV 长度，以英文字符为单位： KEY（Min:16 Max:32 Skip:8），IV（16）。
+        /// </summary>
+        Rijndael,
+        /// <summary>
+        /// 有效的 KEY 与 IV 长度，以英文字符为单位： KEY（Min:16 Max:32 Skip:8），IV（16）。
+        /// </summary>
+        AES
+    }
+
+    /// <summary>
+    /// 加密扩展。
+    /// </summary>
+    public static class CryptoExtentions
+    {
+        private static SymmetricAlgorithm GetSymmetricAlgorithm(CryptoKind kind)
+        {
+            SymmetricAlgorithm algorithm = null;
+
+            switch (kind)
+            {
+                case CryptoKind.DES:
+                    algorithm = DES.Create();
+                    break;
+                case CryptoKind.TripleDES:
+                    algorithm = TripleDES.Create();
+                    break;
+                case CryptoKind.RC2:
+                    algorithm = RC2.Create();
+                    break;
+                case CryptoKind.Rijndael:
+                    algorithm = Rijndael.Create();
+                    break;
+                case CryptoKind.AES:
+                    algorithm = Aes.Create();
+                    break;
+            }
+
+            return algorithm;
+        }
+
+        /// <summary>
+        /// 对称加密（<see cref="CipherMode.ECB"/>，<seealso cref="PaddingMode.PKCS7"/>）。
+        /// </summary>
+        /// <param name="data">内容。</param>
+        /// <param name="key">键。</param>
+        /// <param name="kind">加密方式。</param>
+        /// <returns></returns>
+        public static string Encrypt(this string data, string key, CryptoKind kind = CryptoKind.DES)
+        {
+            if (data is null)
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+
+            if (key is null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+
+            ICryptoTransform crypto = null;
+
+            using (var algorithm = GetSymmetricAlgorithm(kind))
+            {
+                var rgbKey = Encoding.ASCII.GetBytes(key);
+
+                if (!algorithm.ValidKeySize(rgbKey.Length * 8))
+                    throw new ArgumentOutOfRangeException(nameof(key));
+
+                algorithm.Key = rgbKey;
+                algorithm.Mode = CipherMode.ECB;
+                algorithm.Padding = PaddingMode.PKCS7;
+
+                crypto = algorithm.CreateEncryptor();
+            }
+
+            using (var ms = new MemoryStream())
+            {
+                using (var cs = new CryptoStream(ms, crypto, CryptoStreamMode.Write))
+                {
+                    var buffer = Encoding.UTF8.GetBytes(data);
+
+                    cs.Write(buffer, 0, buffer.Length);
+
+                    cs.FlushFinalBlock();
+
+                    cs.Close();
+                }
+
+                return Convert.ToBase64String(ms.ToArray());
+            }
+        }
+
+        /// <summary>
+        /// 对称减密（<see cref="CipherMode.ECB"/>，<seealso cref="PaddingMode.PKCS7"/>）。
+        /// </summary>
+        /// <param name="data">内容。</param>
+        /// <param name="key">键。</param>
+        /// <param name="kind">减密方式。</param>
+        /// <returns></returns>
+        public static string Decrypt(this string data, string key, CryptoKind kind = CryptoKind.DES)
+        {
+            if (data is null)
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+
+            if (key is null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            ICryptoTransform crypto = null;
+
+            using (var algorithm = GetSymmetricAlgorithm(kind))
+            {
+                var rgbKey = Encoding.ASCII.GetBytes(key);
+
+                if (!algorithm.ValidKeySize(rgbKey.Length * 8))
+                    throw new ArgumentOutOfRangeException(nameof(key));
+
+                algorithm.Key = rgbKey;
+                algorithm.Mode = CipherMode.ECB;
+                algorithm.Padding = PaddingMode.PKCS7;
+
+                crypto = algorithm.CreateDecryptor();
+            }
+
+            using (var ms = new MemoryStream())
+            {
+                using (var cs = new CryptoStream(ms, crypto, CryptoStreamMode.Write))
+                {
+                    var buffer = Convert.FromBase64String(data);
+
+                    cs.Write(buffer, 0, buffer.Length);
+
+                    cs.FlushFinalBlock();
+
+                    cs.Close();
+                }
+
+                return Encoding.UTF8.GetString(ms.ToArray());
+            }
+        }
+
+        /// <summary>
+        /// 对称加密。
+        /// </summary>
+        /// <param name="data">内容。</param>
+        /// <param name="key">键。</param>
+        /// <param name="iv">初始化向量。</param>
+        /// <param name="kind">加密方式。</param>
+        /// <returns></returns>
+        public static string Encrypt(this string data, string key, string iv, CryptoKind kind = CryptoKind.DES)
+        {
+            if (data is null)
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+
+            if (key is null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            if (iv is null)
+            {
+                throw new ArgumentNullException(nameof(iv));
+            }
+
+            ICryptoTransform crypto = null;
+
+            using (var algorithm = GetSymmetricAlgorithm(kind))
+            {
+                var rgbKey = Encoding.ASCII.GetBytes(key);
+
+                var rgbIV = Encoding.ASCII.GetBytes(iv);
+
+                if (!algorithm.ValidKeySize(rgbKey.Length * 8))
+                    throw new ArgumentOutOfRangeException(nameof(key));
+
+                if (algorithm.IV.Length != rgbIV.Length)
+                    throw new ArgumentOutOfRangeException(nameof(iv));
+
+                crypto = algorithm.CreateEncryptor(rgbKey, rgbIV);
+            }
+
+            using (var ms = new MemoryStream())
+            {
+                using (var cs = new CryptoStream(ms, crypto, CryptoStreamMode.Write))
+                {
+                    var buffer = Encoding.UTF8.GetBytes(data);
+
+                    cs.Write(buffer, 0, buffer.Length);
+
+                    cs.FlushFinalBlock();
+
+                    cs.Close();
+                }
+
+                return Convert.ToBase64String(ms.ToArray());
+            }
+        }
+
+        /// <summary>
+        /// 对称减密。
+        /// </summary>
+        /// <param name="data">内容。</param>
+        /// <param name="key">键。</param>
+        /// <param name="iv">初始化向量。</param>
+        /// <param name="kind">减密方式。</param>
+        /// <returns></returns>
+        public static string Decrypt(this string data, string key, string iv, CryptoKind kind = CryptoKind.DES)
+        {
+            if (data is null)
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+
+            if (key is null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            if (iv is null)
+            {
+                throw new ArgumentNullException(nameof(iv));
+            }
+
+            ICryptoTransform crypto = null;
+
+            using (var algorithm = GetSymmetricAlgorithm(kind))
+            {
+                var rgbKey = Encoding.ASCII.GetBytes(key);
+
+                var rgbIV = Encoding.ASCII.GetBytes(iv);
+
+                if (!algorithm.ValidKeySize(rgbKey.Length * 8))
+                    throw new ArgumentOutOfRangeException(nameof(key));
+
+                if (algorithm.IV.Length != rgbIV.Length)
+                    throw new ArgumentOutOfRangeException(nameof(iv));
+
+                crypto = algorithm.CreateDecryptor(rgbKey, rgbIV);
+            }
+
+            using (var ms = new MemoryStream())
+            {
+                using (var cs = new CryptoStream(ms, crypto, CryptoStreamMode.Write))
+                {
+                    var buffer = Convert.FromBase64String(data);
+
+                    cs.Write(buffer, 0, buffer.Length);
+
+                    cs.FlushFinalBlock();
+
+                    cs.Close();
+                }
+
+                return Encoding.UTF8.GetString(ms.ToArray());
+            }
+        }
+
+        /// <summary>
+        /// MD5加密(32个字符)。
+        /// </summary>
+        /// <param name="data">数据。</param>
+        /// <param name="encoding">编码，默认：UTF8。</param>
+        /// <param name="toUpperCase">是否转为大小。</param>
+        /// <returns></returns>
+        public static string Md5(this string data, Encoding encoding = null, bool toUpperCase = true)
+        {
+            if (data is null)
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+
+            byte[] buffer = null;
+
+            using (var md5 = MD5.Create())
+            {
+                buffer = md5.ComputeHash((encoding ?? Encoding.UTF8).GetBytes(data));
+            }
+
+            var sb = new StringBuilder();
+
+            foreach (var item in buffer)
+            {
+                sb.Append(item.ToString(toUpperCase ? "X2" : "x2"));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
