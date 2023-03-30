25df78dd8fc674a624b7111ee538f8036044e6ba
影子和树 <m.hyl@foxmail.com>
2023-03-30 16:19:48 +0800

    MCStartup测试。

---
+++ tests/Inkslab.Map.Tests/TestProfile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inkslab.Map.Tests
+{
+    public class TestProfile : Profile
+    {
+        private readonly IConfiguration configuration;
+
+        public TestProfile(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+    }
+}
